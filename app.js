2fc04c11c7681f3690c3c19eb3ad4a0fe76f3efb
Assaf Stone <1774556+devopsjester@users.noreply.github.com>
2023-07-18 13:59:20 +0000

    using yargs

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 const geocode = require('./geocode');
 const weather = require('./weather');
+const yargs = require('yargs');
 
-const location = process.argv[2] || '10001';
+const options = yargs
+  .usage('Usage: $0 [location]')
+  .option('zipcode', {
+    alias: 'z',
+    describe: 'Get weather for a zip code',
+    type: 'string'
+  })
+  .option('city', {
+    alias: 'c',
+    describe: 'Get weather for a city and state',
+    type: 'string'
+  })
+  .help('h')
+  .alias('h', 'help')
+  .argv;
 
-async function getWeather(location) {
-    let latitude, longitude;
-    if (isNaN(location)) {
-        const { lat, long } = await geocode.getLatLongFromCityState(location);
-        latitude = lat;
-        longitude = long;
-    } else {
-        const { lat, long } = await geocode.getLatLongFromZip(location);
-        latitude = lat;
-        longitude = long;
-    }
-    const temperature = await weather.getCurrentWeather(latitude, longitude);
-    console.log(`The temperature in ${location} is ${temperature}F.`);
+async function getWeather() {
+  let latitude, longitude;
+  if (options.zipcode) {
+    const { lat, long } = await geocode.getLatLongFromZip(options.zipcode);
+    latitude = lat;
+    longitude = long;
+  } else if (options.city) {
+    const { lat, long } = await geocode.getLatLongFromCityState(options.city);
+    latitude = lat;
+    longitude = long;
+  } else {
+    console.error('Please specify a zip code or city and state');
+    return;
+  }
+  const temperature = await weather.getCurrentWeather(latitude, longitude);
+  console.log(`The temperature is ${temperature}F.`);
 }
 
-getWeather(location);
-
-
+getWeather();
